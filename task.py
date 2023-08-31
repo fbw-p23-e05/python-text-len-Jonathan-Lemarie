b7f210a4861a33b69c3c6730be6f3e57c81a50f0
Jonathan-Lemarie <jonathan.maheux-lemarie@dci-student.org>
2023-08-31 14:05:17 +0200

    task done

---
+++ task.py
@@ -0,0 +1,17 @@
+
+text1 = input('enter your setence here:')
+
+
+
+if len(text1) % 2 == 0:
+    print('even')
+else:
+    print( 'odd')
+
+
+
+
+# if ord(text1.count % 2 == 0):
+#     print('even')
+# else:
+#     print('odd')
